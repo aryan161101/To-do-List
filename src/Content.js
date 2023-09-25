075a55fcb2f4e4427790c3faae250c8d834fc04e
aryan161101 <aryayan5@gmail.com>
2023-09-25 10:47:49 +0530

    First Commit

---
+++ src/Content.js
@@ -0,0 +1,18 @@
+// ̥import React from 'react'
+// import ItemList from "./Itemlist"
+import ItemList from "./ItemList"
+ const Content = ({items,handleCheck,handleDelete}) => {
+  return (
+    <main>
+      {items.length ? (
+      <ItemList items={items} 
+      handleCheck ={handleCheck}
+      handleDelete ={handleDelete}
+      />
+      ):(
+        <p style={{marginTop: '2rem'}}>Your list is empty</p>
+      )}    
+    </main>
+  )
+}
+export default Content
